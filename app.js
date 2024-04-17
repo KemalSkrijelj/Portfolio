8c82a0736ada07afdd72d46540b4252c3f172e7e
Kemal Škrijelj <kemalskrijelj6@gmail.com>
2024-04-17 23:59:07 +0200

    process

---
+++ app.js
@@ -0,0 +1,12 @@
+document.getElementById("btn").onclick = function () {
+  location.href = "file:///C:/Users/computer/Desktop/html-css-course/Travelly/index.html";
+};
+document.getElementById("btn_photography").onclick = function () {
+  location.href = "file:///C:/Users/computer/Desktop/html-css-course/Gallery/index.html";
+};
+document.getElementById("btn_gallery").onclick = function () {
+  location.href = "file:///C:/Users/computer/Desktop/Kemal%20Skrijelj/dropDownMenu/index.html";
+};
+document.getElementById("btn_youtube").onclick = function () {
+  location.href = "file:///C:/Users/computer/Desktop/html-css-course/Youtube-exercise/youtube.html";
+};
