3fa87a5e8a82c7c67655d40893169e97ba9fb046
Kemal Škrijelj <kemalskrijelj6@gmail.com>
2024-04-18 18:34:58 +0200

    Last step

--- app.js
+++ app.js
@@ -10,3 +10,12 @@ document.getElementById("btn_gallery").onclick = function () {
 document.getElementById("btn_youtube").onclick = function () {
   location.href = "file:///C:/Users/computer/Desktop/html-css-course/Youtube-exercise/youtube.html";
 };
+document.getElementById("gitHub").onclick = function () {
+  location.href = "https://github.com/KemalSkrijelj"
+}
+document.getElementById("instagram").onclick = function () {
+  location.href = "https://www.instagram.com/kemal.skrijelj/"
+}
+document.getElementById("linkedin").onclick = function () {
+  location.href = "https://www.linkedin.com/in/kemal-%C5%A1krijelj-b72785218/"
+}
