364c664a9845bd9fbefbb6291fec8deae7c080d3
Kemal Škrijelj <kemalskrijelj6@gmail.com>
2024-04-26 20:36:24 +0200

    Responsive max-width:450px

--- app.js
+++ app.js
@@ -30,3 +30,19 @@ document.querySelector('.popup.img span').onclick = () => {
   document.querySelector('.popup-img').style.display = 'none';
 
 }
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
